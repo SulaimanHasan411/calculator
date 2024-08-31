3e2dad5e5de9ba137b6cce7cf0be1a20b3ec8019
Sulaiman <SulaimanHasan411@gmail.com>
2024-08-31 01:10:12 -0400

    add functions for arithmetic operations, and an operate function to call the correct operator function

---
+++ script.js
@@ -0,0 +1,34 @@
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+let firstNum;
+let secondNum;
+let operator;
+
+function operate(num1, operator, num2) {
+    if (operator === "+"){
+        return add(num1,num2);
+    }
+    else if (operator === "-"){
+        return subtract(a,b);
+    }
+    else if (operator === "*"){
+        return multiply(num1,num2);
+    }
+    else if (operator === "/"){
+        return divide(num1,num2);
+    }
+}
