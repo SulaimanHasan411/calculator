be0fdae0dc96861041b5ecc2fd8c408ef791d526
Sulaiman <SulaimanHasan411@gmail.com>
2024-08-31 21:11:03 -0400

    add events so that calculationis display output when buttons clicked

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const display = document.querySelector(".display");
+let val = "0";
+
+let firstNum = null;
+let secondNum = null;
+let operator = null;
+
 function add(a,b) {
     return a + b;
 }
@@ -19,22 +26,26 @@ function negative(a) {
 }
 
 function percentage(a) {
-    return a / 100;
+    return round(a / 100);
 }
 
 function round(a) {
-    return Math.round(a * 1000) / 1000;
+    return Math.round(a * 100000) / 100000;
 }
 
 function divide(a,b) {
     if (b === 0){
         alert("Cant divide by 0!");
-        return;
+        return "no";
     }
-    return a/b;
+    return round(a/b);
 }
 
 function mod(a,b) {
+    if (b === 0){
+        alert("Cant divide by 0!");
+        return "no";
+    }
     return a % b;
 }
 
@@ -42,21 +53,108 @@ function exp(a,b) {
     return a ** b;
 }
 
-let firstNum;
-let secondNum;
-let operator;
-
 function operate(num1, operator, num2) {
-    if (operator === "+"){
-        return add(num1,num2);
+    switch(operator){
+        case "+":
+            return add(num1,num2);
+            break;
+        case "-":
+            return subtract(num1,num2);
+            break;
+        case "*":
+            return multiply(num1,num2);
+            break;
+        case "/":
+            return divide(num1,num2);
+            break;
+        case "mod":
+            return mod(num1,num2);
+            break;
+        case "^":
+            return exp(num1,num2);
     }
-    else if (operator === "-"){
-        return subtract(a,b);
+}
+
+function singleOperator(num1,operator){
+    switch(operator){
+        case "+/-":
+            val = negative(num1);
+            display.textContent = val;
+            break;
+        case "%":
+            val = percentage(num1);
+            display.textContent = val;
+            break;
     }
-    else if (operator === "*"){
-        return multiply(num1,num2);
+}
+
+
+function updateDisplay() {
+    if (val.length > 15){
+        val = val.substring(0,15);
     }
-    else if (operator === "/"){
-        return divide(num1,num2);
+    display.textContent = val;
+}
+
+function updateInput(num) {
+    if (val === "0"){
+        val = "";
+        val += num;
     }
+    else {
+        val += num;
+    }
+}
+
+function storeInput(op){
+    operator = op;
+    firstNum = Number(val);
+    val = "";
+    display.textContent = op;
+}
+
+function calculateResult(){
+    secondNum = Number(val);
+    output = operate(firstNum,operator,secondNum);
+    if (output === "no"){
+        clearDisplay();
+        return;
+    }
+    val = "";
+    val += output;
+    display.textContent = val;
+    secondNum = null;
+    operator = null;
+    updateDisplay();
+}
+
+function clearDisplay() {
+    val = "0";
+    display.textContent = val;
+    firstNum = null;
+    operator = null;
+    secondNum = null;
+}
+
+const buttons = document.querySelectorAll("button");
+
+for (const btn of buttons){
+    btn.addEventListener("click",function() {
+        if (btn.classList.contains("num")){
+            updateInput(btn.textContent);
+            updateDisplay();
+        }
+        else if (btn.classList.contains("clear")){
+            clearDisplay();
+        }
+        else if(btn.classList.contains("single")){
+            singleOperator(Number(val),btn.textContent);
+        }
+        else if(btn.classList.contains("operator")){
+            storeInput(btn.textContent);
+        }
+        else if(btn.classList.contains("equals")){
+            calculateResult();
+        }
+    });
 }
